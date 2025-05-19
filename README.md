a8339978313fb9d29b8dd5e42982434208d17954
edy-m <95467047+edy-m@users.noreply.github.com>
2025-05-19 00:03:42 -0700

    final commit, finished

---
+++ README.md
@@ -0,0 +1,4 @@
+GITHUB link: https://github.com/edy-m/ecs162-project3.git
+
+tested and works on dev mode 
+wasnt able to find partner in time since i realized a couple days before due date and it was too late to look as i had already made a lot of progress so i worked on it alone
--- app.py
+++ app.py
@@ -33,17 +33,34 @@ oauth.register(
     client_kwargs={'scope': 'openid email profile'}
 )
 
+# geeksforgeeks - explanation and syntax/structure help for implementing defaultdict
+#https://www.geeksforgeeks.org/defaultdict-in-python/
+# digitalocean - return render_template
+#https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application
+
+#added functionality to when we load home
 @app.route('/')
 def home():
+    #allows us to access current user data so we can use them in our code
     user = session.get('user')
+    #load all the comments in db and form a dict using imported defaultdict from collections library
     allComments = list(COMMENTS.find())
+    #we use default dict to group the comments so we can later access them based on articleNum
     commDictionary = defaultdict(list)
     for comm in allComments:
+        #the following lines convert the id automatically given by mongodb to strings instead of objects
+        #so we can use in our html
         comm['_id'] = str(comm['_id'])
         articleNumber = comm.get('articleNum')
+        #if comment is a reply to a comment
         if comm.get('root'):
             comm['root'] = str(comm['root'])
+        #add entry to dictionary
         commDictionary[articleNumber].append(comm)
+
+    #the below commented out code would automatically make us sign in but that isnt necessary
+    #since if you attempt to comment/reply you will be automatically asked/required to login from there
+    #renders the html page using the user data and comments stored
     return render_template('index.html', user=user, comments=commDictionary)
     #if user:
         #return f"<h2>Logged in as {user['email']}</h2><a href='/logout'>Logout</a>"
@@ -73,11 +90,20 @@ def logout():
     session.clear()
     return redirect('/')
 
+#digital ocean - post and delete comment structure and syntax
+#https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application
+
+#geeks for geeks - function formation/syntax/structure
+#https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/
+
+#new post comment function used by html to post a comment
 @app.route('/post-comment', methods=['POST'])
 def post_comment():
+    #redirect user to login if not logged in and tries to comment
     user = session.get('user', {})
     if not user:
         return redirect('/login')
+    #init user data
     commentContent = request.form['comment']
     articleNum = request.form['articleNum']
     root = request.form.get('root')
@@ -85,17 +111,20 @@ def post_comment():
         "user_name": user.get('name'),
         "comment": commentContent,
         "articleNum": articleNum,
-        "root": root,
-        "removed": False
+        "root": root,       #is the comment a normal comment or a reply to a comment
+        "removed": False    #has comment been deleted?
     }
 
     COMMENTS.insert_one(comment)
 
     return redirect('/')
 
+# pyMongo help - help using pyMongo functions and understanding hoiw they work such as $set and update.one()
+#https://pymongo.readthedocs.io/en/stable/api/pymongo/collection.html#pymongo.collection.Collection.delete_one
 @app.post('/<id>/delete/')
 def delete(id):
     #COMMENTS.delete_one({"_id": ObjectId(id)})
+    #instead of straight up deleteing a comment entirely from db we just change its contents
     COMMENTS.update_one({"_id": ObjectId(id)}, {"$set": {'comment': "comment was removed by moderator", 'removed': True}})
     return redirect('/')
 
